996597dcd28c792cb9bc66a904331e700fa9f947
Tiago Lourinho <lourinho.tiago@gmail.com>
2022-10-31 16:36:21 -0300

    feat: fixation exercise 5

---
+++ Fixation05/CurrencyConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace FixationExercises
+{
+    class CurrencyConverter
+    {
+        public static double iof = 6.00;
+
+        public static double Converter(double quote, double currency)
+        {
+            double dollarQuote = quote + (quote * (iof / 100));
+            double cost = currency * dollarQuote;
+
+            return cost;
+        }
+    }
+}
